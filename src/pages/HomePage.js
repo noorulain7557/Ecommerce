7a4b389bf557ef9806c8a890d760f3b8e6d0f8d8
noorulain7557 <noorulain7557@gmail.com>
2026-07-16 23:45:17 +0500

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,6 +8,9 @@ import { useCart } from "../context/cart";
 import { toast } from "react-toastify";
 import '../styles/Homepage.css';
 
+// Importing the banner image directly so the build system outputs it correctly in production
+import bannerImg from "../images/banner.png";
+
 const HomePage = () => {
   const navigate = useNavigate();
   const [cart, setCart] = useCart();
@@ -106,7 +109,7 @@ const HomePage = () => {
   return (
     <Layout title={"All Products - Best offers "}>
       <img
-        src="/images/banner.png"
+        src={bannerImg}
         className="banner-img"
         alt="Homepage Banner"
       />
